28fa737ac575224dd80d59bf2b30a842d31f7d16
Jonathan Stewart <jonathanst@jonathans-mbp.home>
2015-10-30 22:49:10 +0000

    adding christmas menu and contact us page

--- routes.py
+++ routes.py
@@ -7,18 +7,21 @@ app.secret_key = 'my precious'
 
 @app.route('/')
 def home():
-	return render_template('home.html')
-
+    return render_template('home.html')
 
 @app.route('/menu')
 def menu():
-	return render_template('menu.html')
+    return render_template('menu.html')
 
 @app.route('/gallery')
 def gallery():
-	return render_template('gallery.html')
+    return render_template('gallery.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 
 if __name__ == '__main__':
-	app.debug = True
-	app.run()
+    app.debug = False
+    app.run()
